a682ce3779d47c5384696a411eea6713fc689b3a
framoran <david.framorando@gmail.com>
2021-12-02 16:36:05 +0100

    implemented the controller create action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,19 @@ class UsersController < ApplicationController
   end
 
   def create
+
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to root_path
+    else
+      format.html { render :new }
+    end
+
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :last_name, :email, :password)
   end
 end
