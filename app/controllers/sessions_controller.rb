e9ea9861a737afb6d7bae1d348b77a2059869ead
framoran <david.framorando@gmail.com>
2021-12-02 16:55:44 +0100

    implemented login system

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,27 @@
 class SessionsController < ApplicationController
+
+  def index
+  end
+
   def new
   end
 
   def create
+
+    user = User.find_by(email: params[:email])
+
+    if(user.nil? || !user.authenticate(params[:password]))
+     flash[:alert] = "Email or password is invalid.  Please try again"
+     render 'new'
+    else
+     session[:user_id] = user.id
+     redirect_to root_path
+    end
+
   end
 
-  def login
-  end
-
-  def welcome
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   root to: 'home#index'
-  
+
   resources :sessions
   resources :users
   resources :elements
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
     resources :comments, only: [:new, :create]
     resources :elements
   end
+
+  get 'login', to:'sessions#new'
+
+  get 'signup', to:'users#new'
 
 end
