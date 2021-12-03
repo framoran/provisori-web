63cc0710453802153dc74caf16153aeeaff2ec99
framoran <david.framorando@gmail.com>
2021-12-03 08:48:45 +0100

    add rules to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,15 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :email, presence: true,
+                    uniqueness: true
+
+  before_validation :downcase_email
+
+  private
+
+  def downcase_email
+    self.email = email.downcase
+  end
+
 end
