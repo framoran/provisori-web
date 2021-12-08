06bb0fa3466d1ab0a064bea05431389e1f409819
framoran <david.framorando@gmail.com>
2021-12-08 07:43:17 +0100

    added default image for users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def default_avatar(user)
+    if !user.avatar.present?
+      return "https://68.media.tumblr.com/7d65a925636d6e3df94e2ebe30667c29/tumblr_nq1zg0MEn51qg6rkio1_500.jpg"
+    end
+  end
+
 end
