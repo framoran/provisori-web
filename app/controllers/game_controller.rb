e68a0228d8faa84df61d3cb5248ee6b921e9168a
framoran <david.framorando@gmail.com>
2021-12-10 17:39:36 +0100

    create the routes for the game

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,7 +1,9 @@
 class GameController < ApplicationController
+
   def new
   end
 
   def create
   end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   resources :elements
   resources :comments
   resources :home
+  resources :game
 
   resources :articles do
     resources :comments
