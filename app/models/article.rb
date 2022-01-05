024d8fd95b5303677199019fc2f104cf28e85b4d
framoran <david.framorando@gmail.com>
2022-01-05 11:55:17 +0100

    adjust search scope to identify likes at the beginning of the task

--- app/models/article.rb
+++ app/models/article.rb
@@ -10,9 +10,11 @@ class Article < ApplicationRecord
 
   mount_uploader :image, ImageUploader
 
-  scope :title_contains,       ->(term) { where('title LIKE ?', "%#{term}%".downcase) }
+  scope :title_contains,       ->(term) { where( 'title LIKE ?', "%#{term}%".downcase ) }
+  scope :title_contains_init,       ->(term) { where( 'title LIKE ?', "#{term}%".downcase ) }
+  scope :title_contains_end,       ->(term) { where( 'title LIKE ?', "%#{term}".downcase ) }
   scope :is_published, ->() { where('published = ?', true) }
-  scope :search, ->(term) { title_contains(term).and(is_published()) }
+  scope :search, ->(term) { title_contains(term).and(is_published()).or(title_contains_init(term).and(is_published())).or(title_contains_end(term).and(is_published())) }
 
   def liked?(user)
     !!self.likes.find{|like| like.user_id == user.id}
