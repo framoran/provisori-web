b650beca1fc02d74d39cb6ff8d23c91cc44a695a
David Framorando <41962591+framoran@users.noreply.github.com>
2023-03-23 16:40:38 +0100

    Delete carrierwave.rb

--- config/initializers/carrierwave.rb
+++
@@ -1,8 +0,0 @@
-# config/initializers/carrierwave.rb
-require 'carrierwave/orm/activerecord'
-
-CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-  }
-end
